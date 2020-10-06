d6c468953ed7db57c46ff9e9c9213130a84a3971
Vanderson Nogueira <vanderson.01@hotmail.com>
2020-10-06 11:18:51 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/casa/CursomcApplication.java
@@ -0,0 +1,15 @@
+package br.com.casa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+//	Executar o aplicativo do springboot
+	// Ja vem com um tom cat embedded
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}

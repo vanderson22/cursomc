e4f929a804e4ee70c2157a811cdd1d1ffe01a294
Vanderson Nogueira <vanderson.01@hotmail.com>
2020-10-06 11:47:00 -0300

    Classe Dominio - Categoria add

--- src/main/java/br/com/casa/resources/CategoriaResources.java
+++ src/main/java/br/com/casa/resources/CategoriaResources.java
@@ -1,8 +1,13 @@
 package br.com.casa.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.casa.dominio.Categoria;
 
 /**
  * Recurso de Categoria
@@ -15,8 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Listar Funcionando";
+	public List<Categoria> listar() {
+		Categoria c = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(1, "Banho");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c);
+		lista.add(c2);
+
+		return lista;
 	}
 
 }

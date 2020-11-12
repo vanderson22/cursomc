0835860af41360f8fe2880a02714bb7a74d49b4b
Vanderson Nogueira <vanderson.01@hotmail.com>
2020-11-12 18:28:26 -0300

    Cors custom header

--- src/main/java/br/com/casa/resources/AuthResource.java
+++ src/main/java/br/com/casa/resources/AuthResource.java
@@ -2,13 +2,13 @@ package br.com.casa.resources;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.casa.services.ProdutoService;
 import br.com.casa.services.UsuarioService;
 import br.com.casa.services.security.DetalhesDeUsuario;
 import br.com.casa.services.security.JWTUtil;
@@ -23,6 +23,7 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwt;
 
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(AuthResource.class);
 	/**
 	 * 
 	 *
@@ -32,7 +33,8 @@ public class AuthResource {
 	
 		DetalhesDeUsuario usuario = UsuarioService.autenticado();
 		String token = jwt.generateToken(usuario.getUsername());
-		
+		 log.info("Authorization: " +  "Bearer " + token);
+		 
 		response.addHeader("Authorization", "Bearer " + token);
 
 		return ResponseEntity.noContent().build();
--- src/main/java/br/com/casa/services/filter/ExposeHeaders.java
+++ src/main/java/br/com/casa/services/filter/ExposeHeaders.java
@@ -16,16 +16,18 @@ import org.springframework.stereotype.Component;
  * 
  *  Criado apenas para expor o header location
  *  é automaticamente adicionado como componente @Component
+ *  
+ *  Essa classe é muito importante pois resolve o problema de cors
  * */
 @Component
-public class ExporHeader implements Filter{
+public class ExposeHeaders implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 	    
 		      HttpServletResponse res = (HttpServletResponse) response;
-		       res.addHeader("access-control-expose-headers" , "location" );
+		       res.addHeader("access-control-expose-headers" , "*, location, Authorization" );
 		          chain.doFilter(request, response);
 		
 	}
